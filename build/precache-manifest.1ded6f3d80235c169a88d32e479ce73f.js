26966ed2d7e6df9aa449610853d2e2bedc4f4c31
Karthik shetty <shettykarthik200@gmail.com>
2022-04-12 18:49:43 +0530

    Added source

---
+++ build/precache-manifest.1ded6f3d80235c169a88d32e479ce73f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bd3c67c55e5f6d7ad62",
+    "url": "/static/css/main.3a5fbf51.chunk.css"
+  },
+  {
+    "revision": "3bd3c67c55e5f6d7ad62",
+    "url": "/static/js/main.3bd3c67c.chunk.js"
+  },
+  {
+    "revision": "a322975c65224bc82c97",
+    "url": "/static/js/1.a322975c.chunk.js"
+  },
+  {
+    "revision": "790c44284cfa2c06cc32",
+    "url": "/static/js/2.790c4428.chunk.js"
+  },
+  {
+    "revision": "c4991780b72d3192e4bc",
+    "url": "/static/js/runtime~main.c4991780.js"
+  },
+  {
+    "revision": "48f8427b64db314cd2d455bbbae6041d",
+    "url": "/index.html"
+  }
+];
