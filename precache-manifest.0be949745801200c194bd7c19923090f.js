ac1988ae8612f85e904521e790209cdbf88e160f
Karthik shetty <shettykarthik200@gmail.com>
2022-04-12 19:26:59 +0530

    Updates

---
+++ precache-manifest.0be949745801200c194bd7c19923090f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49899acd9a30fb59f4ba",
+    "url": "/React-project/static/css/main.3a5fbf51.chunk.css"
+  },
+  {
+    "revision": "49899acd9a30fb59f4ba",
+    "url": "/React-project/static/js/main.49899acd.chunk.js"
+  },
+  {
+    "revision": "a322975c65224bc82c97",
+    "url": "/React-project/static/js/1.a322975c.chunk.js"
+  },
+  {
+    "revision": "790c44284cfa2c06cc32",
+    "url": "/React-project/static/js/2.790c4428.chunk.js"
+  },
+  {
+    "revision": "1261a355dc1f340bb023",
+    "url": "/React-project/static/js/runtime~main.1261a355.js"
+  },
+  {
+    "revision": "5fa2bde46170ecc974e20f0ba8442650",
+    "url": "/React-project/index.html"
+  }
+];
